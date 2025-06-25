aefdbf3c56f8e1f5d26c21c26484754ec14c3c2c
PRAKASH ANAND <134529932+prakashanand084@users.noreply.github.com>
2025-06-25 16:35:51 +0530

    Update frontend-task-service.tf

--- ecs-task/frontend-task-service.tf
+++ ecs-task/frontend-task-service.tf
@@ -61,7 +61,7 @@ resource "aws_ecs_service" "front_service" {
   name            = "frontend-service"
   cluster         = aws_ecs_cluster.ecs_cluster.id
   task_definition = aws_ecs_task_definition.front_task.arn
-  desired_count   = 1
+  desired_count   = 10
   launch_type     = "FARGATE"
 
   network_configuration {
